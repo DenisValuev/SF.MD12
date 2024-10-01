61725ae1955735593ff9934f56bd905106c8261c
DenisValuev <valuev.denis91@mail.ru>
2024-10-01 16:52:33 +0300

    SF.Module12.Unit2-master: add new Unit2

---
+++ SF.Module12.Unit2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace SF.Module12.Unit2
+{
+    public class User 
+    {
+        public bool LoggedId;
+        public bool HasAccessToSection;
+        public bool HasUnseenNews;
+        public bool HasAdminRigths;
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static void GetPageForUser(User user)
+        {
+            if (!user.LoggedId)
+            {
+                return;
+            }
+            Console.WriteLine("User logged in");
+
+            if (!user.HasAccessToSection)
+            {
+                return;
+            }
+            Console.WriteLine("User has access to that section");
+
+            if (user.HasUnseenNews)
+            {
+            }
+
+            if (user.HasAdminRigths)
+            {
+
+            }
+
+        }
+    }
+}
